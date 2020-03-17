14a70695d0e68b66ad8468259ec2cd49973d284f
李三思 <baibai_lee@163.com>
2020-03-17 14:18:50 +0800

    feat:补充工具函数

--- client/src/lib/utils.ts
+++ client/src/lib/utils.ts
@@ -1,5 +1,12 @@
 import dayjs from 'dayjs'
 
 export const Util = {
-
+  /**
+   * 接受一个日期 对象/字符串/时间戳 返回一个格式化的日期
+   * 
+   * 不传参则默认返回当前的日期
+   */
+  formatDate: (date: Date | string | number | undefined) => {
+    return dayjs(date).format('YYYY-MM-DD hh:mm:ss')
+  }
 }
