4232292ed5713c899a3e2f8d0bb8cdfab6759879
李三思 <baibai_lee@163.com>
2020-03-19 17:09:15 +0800

    feat:添加使用须知页面

--- client/src/pages/routes/login/protocol/index.tsx
+++ client/src/pages/routes/login/protocol/index.tsx
@@ -1,12 +1,25 @@
 import Taro from '@tarojs/taro'
 import { View } from '@tarojs/components'
 
+import { UserProtocol } from '../../../../config'
+
 import './index.scss'
 
 const Protocol: Taro.FC = () => {
   return (
     <View className='protocol'>
-      Protocol
+      <View key='key' className='at-article'>
+        <View className='at-article__h1'>{UserProtocol.title}</View>
+        <View className='at-article__content'>
+          <View className={`at-article__section`}>
+            {
+              UserProtocol.content.map(content => (
+                <View className='at-article__p' key='key'>{content}</View>
+              ))
+            }
+          </View>
+        </View>
+      </View>
     </View>
   )
 }
